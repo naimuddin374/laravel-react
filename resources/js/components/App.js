1dc9b1d7788eff93fcd12332e986312db72f863b
naim.uddin@lubyc.com <naim.uddin@lubyc.com>
2019-10-02 02:09:51 +0600

    This is first commit

---
+++ resources/js/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css'
+
+class App extends React.Component {
+    render() {
+
+        return <h1>Welcome to our app</h1>
+    }
+}
+export default App;
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Laravel</title>
+        <link href="/css/app.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div id="root"></div>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
